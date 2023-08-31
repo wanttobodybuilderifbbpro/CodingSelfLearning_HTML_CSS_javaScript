d8a44856e19f83bf6d3ea3df97adce0fd0bbcb03
박재형 <wogud114@gmail.com>
2023-08-31 17:47:53 +0900

    I solved the practice question

---
+++ JavaScript/Chapter 12/practice/js/practice6.js
@@ -0,0 +1,4 @@
+const btnEl = document.querySelector("button");
+btnEl.ondblclick = function(){
+  alert("It is the WRONG ACCESS!!!");
+}
---
+++ JavaScript/Chapter 12/practice/js/practice6_other.js
@@ -0,0 +1,6 @@
+document.querySelector("button").addEventListener("dblclick", function(){
+  alert("It is the WRONG ACESS!!!");
+})
+
+/* 이베트 등록 대상 노드를 선택하고 addEventListener() 매서드에 이벤트 타입과 
+그에 따른 실행 함수를 매개변수로 전달해 이벤트를 등록하면 됨 */
