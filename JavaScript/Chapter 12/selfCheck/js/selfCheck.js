54104d0c9437f72d4fcc6a67b68c9d93d595693b
박재형 <wogud114@gmail.com>
2023-09-01 18:19:21 +0900

    Il y a fait

--- JavaScript/Chapter 12/selfCheck/js/selfCheck.js
+++ JavaScript/Chapter 12/selfCheck/js/selfCheck.js
@@ -1,3 +1,16 @@
 document.forms[0].addEventListener("submit", function(e){
+
+  // 1 
   e.preventDefault();
+  const idEl = document.forms[0].username;
+  const idValue = idEl.value.trim();
+  const pwEl = document.forms[0].password;
+  const pwValue = pwEl.value.trim();
+
+  // 2
+  if(idValue === ""){
+    alert("Enter the passcodes.");
+    idEl.focus();
+    return;
+  }
 })
